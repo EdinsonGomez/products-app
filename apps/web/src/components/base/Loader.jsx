01427818255807ff04359027550bd6478d389f7f
EdinsonGomez <edinsongomez92@gmail.com>
2024-03-25 23:17:42 -0500

    web:Add productList states

---
+++ apps/web/src/components/base/Loader.jsx
@@ -0,0 +1,10 @@
+function Loader() {
+  return (
+    <div className="flex flex-col justify-center items-center gap-y-2">
+      <div className="loader" />
+      <span className="text-black/70">Cargando...</span>
+    </div>
+  )
+}
+
+export default Loader
--- apps/web/src/components/productsList/ProductsList.jsx
+++ apps/web/src/components/productsList/ProductsList.jsx
@@ -2,11 +2,16 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts } from '@/store/productsSlice';
 import { useEffect } from "react";
 import ProductsItem from "./ProductsItem";
+import Loader from "../base/Loader";
 
 function ProductsList() {
   const dispatch = useDispatch();
 
   const { list } = useSelector((state) => state.products);
+
+  const onRequest = () => {
+    dispatch(fetchProducts());
+  }
 
   useEffect(() => {
     dispatch(fetchProducts());
@@ -14,14 +19,47 @@ function ProductsList() {
   }, []);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-10">
-      {list?.data?.map((product) => (
-        <ProductsItem
-          key={product.id}
-          data={product}
-        />
-      ))}
-    </div>
+    <>
+      {list.status === 'succeeded' && list.data?.length > 0 ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-10">
+          {list?.data?.map((product) => (
+            <ProductsItem
+              key={product.id}
+              data={product}
+            />
+          ))}
+        </div>
+      ) : (
+        <div className="h-full w-full flex items-center justify-center">
+          {list.status === 'loading' && <Loader />}
+
+          {list.status === 'failed' && !!list.error && (
+            <div className="flex flex-col justify-center items-center">
+              <p className="text-rose-500">{list.error ?? ''}</p>
+              <span
+                className="material-symbols-outlined text-black/50 hover:cursor-pointer"
+                onClick={onRequest}
+              >
+                refresh
+              </span>
+            </div>
+
+          )}
+
+          {list.status === 'succeeded' && list.data?.length === 0 && (
+            <div className="flex flex-col items-center">
+              <p>No hay productos que mostrar en este momento</p>
+              <span
+                className="material-symbols-outlined text-black/50 hover:cursor-pointer"
+                onClick={onRequest}
+              >
+                refresh
+              </span>
+            </div>
+          )}
+        </div>
+      )}
+    </>
   )
 }
 
